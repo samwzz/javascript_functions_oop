0b37d15c0265cc5f68bb44ea0c9f54eb51b8157e
Sam Wang <sam.zz.wang@gmail.com>
2017-04-27 13:55:14 -0700

    Complete clock

---
+++ clock.js
@@ -0,0 +1,32 @@
+class Clock {
+  constructor() {
+    const date = new Date();
+    this.hours = date.getHours();
+    this.minutes = date.getMinutes();
+    this.seconds = date.getSeconds();
+    setInterval(this._tick.bind(this), 1000);
+  }
+
+  printTime() {
+    console.log(`${this.hours}:${this.minutes}:${this.seconds}`);
+  }
+
+  _tick() {
+    this.seconds++;
+    if (this.seconds === 60) {
+      this.seconds = 0;
+      this.minutes++;
+    }
+    if (this.minutes === 60) {
+      this.minutes = 0;
+      this.hours++;
+    }
+    if (this.hours === 24){
+      this.hours = 0;
+    }
+    this.printTime();
+  }
+
+}
+
+const clock = new Clock();
